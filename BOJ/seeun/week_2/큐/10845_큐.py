92bf75c5c9d1245845a032c7aa57e71f33d7648f
seeun-320 <seorang0320@gmail.com>
2023-03-14 00:29:24 +0900

    git commit -m "[BOJ] 큐(10845) / 실버4 / ?분" -m "https://www.acmicpc.net/problem/10845"

---
+++ BOJ/seeun/week_2/큐/10845_큐.py
@@ -0,0 +1,43 @@
+import sys 
+n = int(input())
+
+queue = []
+
+for i in range(n):
+    cmd = sys.stdin.readline().rstrip()
+    if len(cmd)>5:
+        cmd, x = cmd.split()
+    
+    if cmd == "push":
+        queue.append(x)
+    elif cmd == "pop":
+        if len(queue) != 0:
+            print(queue[0])
+            del queue[0]
+        else:
+            print(-1)
+            res.append(-1)
+    elif cmd == "size":
+        print(len(queue))
+        res.append(-1)
+    elif cmd == "empty":
+        if len(queue) == 0:
+            print(1)
+            res.append(-1)
+        else:
+            print(0)
+            res.append(-1)
+    elif cmd == "front":
+        if len(queue) != 0:
+            print(queue[0])
+            res.append(-1)
+        else:
+            print(-1)
+            res.append(-1)
+    elif cmd == "back":
+        if len(queue) != 0:
+            print(queue[len(queue)-1])
+            res.append(queue[len(queue)-1])
+        else:
+            print(-1)
+            res.append(-1)
