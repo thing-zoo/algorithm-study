f5591df2487ce29af1ebd7e2257b7ce2042db9c7
seeun-320 <seorang0320@gmail.com>
2023-03-24 03:44:19 +0900

    git commit -m  "[MOD] 텀 프로젝트(9466) / 주석 추가"

--- BOJ/seeun/week_3/BFS, DFS/9466_텀프로젝트.py
+++ BOJ/seeun/week_3/BFS, DFS/9466_텀프로젝트.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.setrecursionlimit(200000)
+sys.setrecursionlimit(200000) # 크기 안 늘려주면 채점할때 오류남
 input = sys.stdin.readline
 
 stack = []
@@ -11,14 +11,12 @@ def dfs(me, mypick):
     stack.append(me)
     visited[me] = 1
 
-    if visited[mypick] == 1:
+    if visited[mypick] == 1: # 이미 팀 짜는데 들어가잇으면
         if mypick in stack:
-            cnt += len(stack[stack.index(mypick):])
+            cnt += len(stack[stack.index(mypick):]) # 팀 완성 된 만큼만 자르기
             return
     else:
         dfs(mypick, stud[mypick])
-
-
 
 n = int(input())
 
@@ -29,13 +27,12 @@ for _ in range(n):
     stud_num = int(input())
     visited = [0] * (stud_num+1)
 
-
     stud = [0]
     stud.extend(list(map(int, input().split())))
 
     cnt = 0
     for i in range(1, stud_num+1):
-       if visited[i] == 0:
+       if visited[i] == 0: # 방문 안한거면 팀원 만들러 가기
             stack = []
             dfs(i, stud[i])
     print(stud_num-cnt)
