520b4dbcc6a153d325fe96aa3637cae8b4993d6a
seeun-320 <seorang0320@gmail.com>
2023-04-01 01:07:04 +0900

    git commit -m "[BOJ] 에디터(1406) / 실버2 / 20분” "https://www.acmicpc.net/problem/1406
    스택 이용
    
    첫째주에 아무 자료구조도 공부하지 않고 풀었을 때는 시간초과가 났었는데 스택 등을 공부한 뒤에 풀고나니까 아주 쉬운 문제였다.
    
    역시 사람은 공부를 해야하나봐"

--- BOJ/seeun/week_1/연결리스트/1406_에디터.py
+++ BOJ/seeun/week_1/연결리스트/1406_에디터.py
@@ -1,26 +1,28 @@
 import sys
 string = list(sys.stdin.readline().strip())
 n = int(sys.stdin.readline())
-left = string
-right = []
+
+origin = string
+temp = [] # 커서 이동에 따라 옮겨 담아둘 스택
 
 for i in range(n):
     cmd = sys.stdin.readline().strip()
     if len(cmd)>1:
         cmd, s = cmd.split()
-
-    if cmd == "L":
-        right = left[-1:]+right[:]
-        left = left[:-1]
-    elif cmd == "D":
-        left = left[:] + right[:1]
-        right = right[1:]
-    elif cmd == "B":
-        if len(left) > 0:
-            left.pop()
+    
+    # 명령어 수행
+    if cmd == "L": 
+        if origin : # 왼쪽으로 한 칸 갈 수 있으면
+            temp.append(origin.pop()) # 스택에 옮겨두기
+    elif cmd == "D": # 오른족으로 갈 수 있는데
+        if temp: # 커서 오른쪽에도 글자가 있으면
+            origin.append(temp.pop()) # 다시 origin에 옮겨 담음
+    elif cmd == "B": # 지울 거 있으면 그냥 지우기
+        if origin:
+            origin.pop()
     elif cmd == "P":
-        left.append(s)
-    print(left, right)
+        origin.append(s)
 
-
-print(*left,*right, sep="")
+while temp: # 커서가 중간에 있을 경우 temp 스택에 있는것들 다 옮겨 담기
+    origin.append(temp.pop())
+print(*origin, sep="")
