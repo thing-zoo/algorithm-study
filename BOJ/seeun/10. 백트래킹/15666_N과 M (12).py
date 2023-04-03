4482befda9b3064d492ebe545a3c455f41426f54
seeun-320 <seorang0320@gmail.com>
2023-04-03 16:00:50 +0900

    git commit -m "[BOJ] N과 M (12)(15666) / 실버2 / 3분” "https://www.acmicpc.net/problem/15666”

---
+++ BOJ/seeun/10. 백트래킹/15666_N과 M (12).py
@@ -0,0 +1,31 @@
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+
+res = []
+arr = [] # 길이 M인 수열 저장하는 배열
+
+def btk():
+    global res
+    if len(arr) == m: # M개의 숫자 다 골랐으면 출력
+        tmp = arr[:]
+        tmp = tuple(tmp)
+        res.append(tmp)  
+        return
+
+    for i in range(len(nums)):
+        if not arr:
+            arr.append(nums[i])
+            btk()
+            arr.pop()
+        elif arr[-1] <= nums[i]: # 비내림차순을 위한 조건
+            arr.append(nums[i])
+            btk()
+            arr.pop()
+      
+btk()
+setres = set(res) # set으로 중복 제거
+res = list(setres) # 리스트로 변경후 정렬
+res.sort()
+for i in range(len(res)):
+    print(" ".join(map(str, res[i])))
