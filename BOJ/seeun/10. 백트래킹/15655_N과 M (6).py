e4b5c153fe46ea0eb4be06de9d43dfbcabddfea5
seeun-320 <seorang0320@gmail.com>
2023-04-03 14:33:19 +0900

    git commit -m "[BOJ] N과 M (6)(15655) / 실버3 / 5분” "https://www.acmicpc.net/problem/15655
    (5)에서 수열 내의 오름차순 조건이 추가된 문제"

---
+++ BOJ/seeun/10. 백트래킹/15655_N과 M (6).py
@@ -0,0 +1,24 @@
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+nums.sort()
+arr = [] # 길이 M인 수열 저장하는 배열
+
+# visited 체크 대신 arr에 어팬드 팝
+def btk(num):
+    if len(arr) == m: # M개의 숫자 다 골랐으면 출력
+        for i in arr:
+            print(i, end=" ")
+        print()
+        return
+
+    for i in range(len(nums)): # nums 안에서 숫자고르기
+        if not arr:
+            arr.append(nums[i])
+            btk(i+1)
+            arr.pop()
+        elif nums[i] not in arr and arr[-1] < nums[i]: # 오름차순을 위한 조건
+            arr.append(nums[i])
+            btk(i+1)
+            arr.pop()
+
+btk(1)
