3d6f4f6e2f80c8f5a65954c1a08f04682dfffea2
thing-zoo <cathy4025@gmail.com>
2023-04-26 20:32:55 +0900

    [BOJ] 2*n 타일링2 / 실버3 / 10분
    ## 해결방법 및 핵심로직
    dp
    
    2*n타일링에서 2*2가 추가된 문제
    1*2를 두개 놓는것과 2*2를 하나 놓는 것은 같음
    따라서 2배로 처리해줌

---
+++ BOJ/thing-zoo/8.DP/2*n 타일링2.py
@@ -0,0 +1,9 @@
+n = int(input())
+if n < 2:
+    print(1)
+else:
+    dp = [0]*n
+    dp[0] = 1; dp[1] = 3
+    for i in range(2, n):
+        dp[i] = (dp[i-1] + dp[i-2]*2)%10007
+    print(dp[n-1])
