f5d8dfcf5cbdbcf02cd666e31181fbb47bf59734
thing-zoo <cathy4025@gmail.com>
2023-04-28 10:50:00 +0900

    [BOJ] 퇴사2 / 골드5 / 30분
    ## 해결방법 및 핵심로직
    dp
    
    퇴사문제는 n<=15여서 O(n^2)으로 풀수있었지만
    퇴사2는 n<=150만이므로 O(n)으로 풀어야함
    
    dp[i] = i번째날의 최대 수익
    해당 상담일정이 n일 안에 가능하면 상담 진행:
      dp[상담마친날] = max(dp[상담마친날], dp[오늘] + 상담 수익)
    상담을 진행 하지 않을 경우:
      오늘 수익을 다음날로 넘김
      dp[다음날] = max(dp[다음날], dp[오늘])

---
+++ BOJ/thing-zoo/8.DP/퇴사2.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+dp = [0]*(n+1) # dp[i] = i번째날의 최대 수익
+max_profit = 0
+for i in range(n):
+    time, profit = map(int, input().rstrip().split())
+    if i + time <= n: # 상담이 n일안에 가능하면 상담 진행
+        dp[i + time] = max(dp[i + time], dp[i] + profit) # 상담마친 날의 수익과 이번상담을 했을때까지의 수익 비교
+    # 상담을 진행하지 않을 경우
+    dp[i + 1] = max(dp[i + 1], dp[i]) # 다음날의 수익과 현재까지의 수익 비교
+print(dp[n])
