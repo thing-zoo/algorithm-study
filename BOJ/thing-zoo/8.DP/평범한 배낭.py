56115c06d9bfb8afe9f2ead272e25d7cc0dd55de
thing-zoo <cathy4025@gmail.com>
2023-05-01 15:35:11 +0900

    [BOJ] 평범한 배낭 / 골드5 / 연습
    https://www.acmicpc.net/problem/12865
    
    ## 해결방법 및 핵심로직
    0-1 knapsack(dp)
    
    유명한 배낭문제인데 개념익힐겸 다시 정리
    배낭 문제는 그리디로 풀 수 있으나
    이 문제는 분해할 수 없는 배낭문제로 dp로 풀어야함

---
+++ BOJ/thing-zoo/8.DP/평범한 배낭.py
@@ -0,0 +1,14 @@
+n, k = map(int, input().split())
+w, v = [], []
+for _ in range(n):
+    a, b = map(int, input().split())
+    w.append(a); v.append(b)
+
+dp = [[0]*(k+1) for _ in range(n)] # dp[i][j]: 최대 무게가 j일때, i개의 아이템으로 최대가치
+for i in range(n): # 아이템을 순서대로
+    for j in range(1, k+1): # k원까지
+        if j >= w[i]: # 포함할 수있으면
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i]]+v[i]) # 포함하지 않았을때 값과 포함했을때 비교
+        else: # 포함할 수 없으면
+            dp[i][j] = dp[i-1][j] # 그전 값 그대로
+print(dp[n-1][k])
