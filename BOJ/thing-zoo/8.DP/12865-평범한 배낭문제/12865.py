0f9467f1b8f77e4c2cfea5bf933085925cb5549c
thing-zoo <cathy4025@gmail.com>
2023-05-30 18:05:03 +0900

    [BOJ] 평범한 배낭 / 골드5 / 자바

--- BOJ/thing-zoo/8.DP/12865-평범한 배낭문제/12865.py
+++ BOJ/thing-zoo/8.DP/12865-평범한 배낭문제/12865.py
@@ -4,9 +4,9 @@ for _ in range(n):
     a, b = map(int, input().split())
     w.append(a); v.append(b)
 
-dp = [[0]*(k+1) for _ in range(n)] # dp[i][j]: 최대 무게가 j일때, i개의 아이템으로 최대가치
+dp = [[0]*(k+1) for _ in range(n)] # dp[i][j]: 최대 무게가 j일때, i개의 아이템까지 봤을때 최대가치
 for i in range(n): # 아이템을 순서대로
-    for j in range(1, k+1): # k원까지
+    for j in range(1, k+1): # k까지
         if j >= w[i]: # 포함할 수있으면
             dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i]]+v[i]) # 포함하지 않았을때 값과 포함했을때 비교
         else: # 포함할 수 없으면
