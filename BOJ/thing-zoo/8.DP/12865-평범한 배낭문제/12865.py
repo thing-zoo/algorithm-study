1cd8c179325b16d76907136a1c75d9f0d36799ee
thing-zoo <cathy4025@gmail.com>
2023-11-02 10:59:47 +0900

    재채점으로 인한 코드 수정
    i=0일때 i-1때문에 -1이 되어 마지막 값을 비교하게 됨

--- BOJ/thing-zoo/8.DP/12865-평범한 배낭문제/12865.py
+++ BOJ/thing-zoo/8.DP/12865-평범한 배낭문제/12865.py
@@ -1,14 +1,14 @@
 n, k = map(int, input().split())
-w, v = [], []
-for _ in range(n):
-    a, b = map(int, input().split())
-    w.append(a); v.append(b)
+w, v = [0]*(n+1), [0]*(n+1)
+for i in range(1, n+1):
+    w[i], v[i] = map(int, input().split())
 
-dp = [[0]*(k+1) for _ in range(n)] # dp[i][j]: 최대 무게가 j일때, i개의 아이템까지 봤을때 최대가치
-for i in range(n): # 아이템을 순서대로
+# dp[i][j]: 최대 무게가 j일때, i개의 아이템까지 봤을때 최대가치
+dp = [[0]*(k+1) for _ in range(n+1)]
+for i in range(1, n+1): # 아이템을 순서대로
     for j in range(1, k+1): # k까지
-        if j >= w[i]: # 포함할 수있으면
-            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i]]+v[i]) # 포함하지 않았을때 값과 포함했을때 비교
+        if w[i] <= j: # 포함할 수있으면
+            dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i]] + v[i]) # 포함하지 않았을때 값과 포함했을때 비교
         else: # 포함할 수 없으면
             dp[i][j] = dp[i-1][j] # 그전 값 그대로
-print(dp[n-1][k])
+print(dp[n][k])
