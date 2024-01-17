9f00db9cf4cfa9a91b1a0faf42c1ba20aba48112
thing-zoo <cathy4025@gmail.com>
2024-01-17 13:52:14 +0900

    [BOJ] 탑 / 골드5 / 20분(재풀이)

--- BOJ/thing-zoo/2.스택/탑.py
+++ BOJ/thing-zoo/2.스택/탑.py
@@ -1,15 +1,15 @@
 import sys
 
 n = int(sys.stdin.readline())
-data = list(map(int, sys.stdin.readline().split()))
-stack = []
-result = [0] * n
+h = list(map(int, sys.stdin.readline().split())) # 탑들의 높이
+stack = [] # 탑의 인덱스 저장
+answer = [0] * n 
 
-for i in range(n):
-    while stack and data[stack[-1]] < data[i]:
-        stack.pop()
-    if stack:
-        result[i] = stack[-1] + 1
+for i in range(n): # 앞에서부터 돌면서
+    while stack and h[stack[-1]] < h[i]: # 현재탑의 높이보다 작은 탑이면
+        stack.pop() # 제거
+    if stack: # 남은 탑이 있으면
+        answer[i] = stack[-1] + 1 # 탑의 번호 넣기
     stack.append(i)
 
-print(*result)
+print(*answer)
