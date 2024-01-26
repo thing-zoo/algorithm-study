c1a9c3b24bf5ac799dd59cb319d4e9655f0f4968
thing-zoo <cathy4025@gmail.com>
2024-01-26 23:43:40 +0900

    [BOJ] 다음 소수 / 실버4 / 30분
    ## 해결방법 및 핵심로직
    수학
    
    처음에 에라토스테네스의 체로 접근하려고 했는데
    최대값이 너무 커서 시공간복잡도가 너무 커짐
    바로바로 소수판별하는 식으로 해결

---
+++ BOJ/thing-zoo/10.수학/다음 소수.py
@@ -0,0 +1,15 @@
+def is_prime(x):
+    if x < 2: return False
+    for i in range(2, int(x**0.5)+1):
+        if x%i == 0:
+            return False
+    return True
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    while True:
+        if is_prime(n):
+            break
+        n += 1
+    print(n)
