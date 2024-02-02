0c0762917d5b544592e7c9ea7d8294fb227a970c
thing-zoo <cathy4025@gmail.com>
2024-02-02 16:01:31 +0900

    [BOJ] 가장 긴 증가하는 부분 수열 / 실버2 / 10분(재풀이)

--- BOJ/thing-zoo/08.DP/가장 긴 증가하는 부분 수열.py
+++ BOJ/thing-zoo/08.DP/가장 긴 증가하는 부분 수열.py
@@ -1,10 +1,11 @@
+# 최장 증가 부분 수열(LIS: Longest Increasing Subsequence)
+# O(n^2)
 n = int(input())
-data = list(map(int, input().split()))
-dp = [1]*n
-max_length = 1
+a = list(map(int, input().split()))
+# dp[i]=a[i]를 마지막값으로 가지는 LIS의 길이
+dp = [1]*n # 처음 길이는 1로 초기화
 for i in range(1, n):
     for j in range(i):
-        if data[j] < data[i]:
-            dp[i] = max(dp[i], dp[j]+1)
-    max_length = max(max_length, dp[i])
-print(max_length)
+        if a[j] < a[i]: # 증가하는 형태면
+            dp[i] = max(dp[j] + 1, dp[i]) # 기존과 j까지의 LIS 길이 비교
+print(max(dp)) # 그중에서 최대값 반환
