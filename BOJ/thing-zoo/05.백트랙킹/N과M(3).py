9956bf1bdb269aef2749066ef6cf6495859777b4
thing-zoo <cathy4025@gmail.com>
2024-04-12 17:24:16 +0900

    [BOJ] N과M(3) / 실버3 / 재풀이

--- BOJ/thing-zoo/05.백트랙킹/N과M(3).py
+++ BOJ/thing-zoo/05.백트랙킹/N과M(3).py
@@ -1,11 +1,9 @@
-def f():
-    if len(result) == m:
-        print(*result)
+def dfs(res):
+    if len(res) == m:
+        print(*res)
         return
     for i in range(1, n+1):
-        result.append(i)
-        f()
-        result.pop()
+        dfs(res + [i])
+    
 n, m = map(int, input().split())
-result = []
-f()
+dfs([])
