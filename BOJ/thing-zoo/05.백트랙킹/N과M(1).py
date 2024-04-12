366fd7c82f8fd061c0a0b6b5eec70e7432bb3fcb
thing-zoo <cathy4025@gmail.com>
2024-04-12 17:16:58 +0900

    [BOJ] N과M(1) / 실버3 / 재풀이

--- BOJ/thing-zoo/05.백트랙킹/N과M(1).py
+++ BOJ/thing-zoo/05.백트랙킹/N과M(1).py
@@ -1,16 +1,12 @@
-def f():
-    if len(result) == m: # m개를 모두 택했으면 탈출
-        print(*result)
+def dfs(res):
+    if len(res) == m:
+        print(*res)
         return
-    for i in range(1, n+1): # 1~n까지의 수에 대해
-        if not visited[i]:  # 아직 i를 방문안했다면
-            visited[i] = True   # 방문표시
-            result.append(i)    # k번째수를 i로
-            f()                 # 다음수를 정하러
-            visited[i] = False  # 방문표시 취소
-            result.pop()        # 값 다시 빼기
-
+    for i in range(1, n+1):
+        if i not in res:
+            res.append(i)
+            dfs(res)
+            res.remove(i)
+    
 n, m = map(int, input().split())
-result = []
-visited = [False]*(n+1)
-f()
+dfs([])
